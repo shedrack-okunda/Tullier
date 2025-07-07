a51618bb7a41670923710198f91be85c5b20dba7
Sheddy <okundashedrack@gmail.com>
2025-07-07 21:02:28 +0300

    Added types for testimonials, programs, resources, events and services

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export interface Testimonials {
+	name: string;
+	text: string;
+	rating: number;
+}
+
+export interface Programs {
+	title: string;
+	description: string;
+	duration: string;
+	price: string;
+}
+
+export interface Resources {
+	title: string;
+	author: string;
+	price: string;
+	description: string;
+}
+
+export interface Events {
+	title: string;
+	date: Date;
+	time: string;
+	location: string;
+	description: string;
+}
+
+export interface Services {
+	icon: string;
+	title: string;
+	description: string;
+}
